6e37733ebfb8331bbc4d4458ca022915796cc67d
Tarakeshwaran <tarakeshsampath1@gmail.com>
2024-08-13 13:44:13 +0530

    removed unwanted components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,41 @@
 // src/pages/Login.js
-import React from 'react';
-import InputField from '../components/InputField';
-import Button from '../components/Button';
+import React ,{ useState }from 'react';
 import Wrapper from '../components/Wrapper';
 import { Link } from 'react-router-dom';
 import '../styles/Login.css'; // Adjust the path as necessary
 
-const Login = () => (
-  <Wrapper>
-    <div className="login_box">
-      <div className="login-header">
-        <h1><Link to="/" style={{color: 'white'}}>InvCare</Link></h1>
-        <br />
-        <h4>Login</h4>
+const Login = () => {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Username: ${user} Password: ${pass}`);
+  }
+  return(
+    <Wrapper>
+      <div className="login_box">
+        <div className="login-header">
+          <h1><Link to="/" style={{color: 'white'}}>InvCare</Link></h1>
+          <br />
+          <h4>Login</h4>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input_box">
+            <input type="text" id="User" placeholder="username" className='input-field' value={user} onChange={(e)=>setUser(e.target.value)} required/>
+          </div> 
+          <div className="input_box">
+            <input type="password" id="Pass" placeholder="Password" className='input-field' value={pass} onChange={(e)=>setPass(e.target.value)} required/>
+          </div> 
+          <input type="submit" value="Login" className = "input-submit"/>
+        </form>
+        <div className="register">
+          <span>Don't have an account? <Link to="/register">Register</Link></span>
+        </div>
       </div>
-      <InputField type="text" id="user" placeholder="username" />
-      <InputField type="password" id="pass" placeholder="Password" />
-      <Button type="submit" value="Login" />
-      <div className="register">
-        <span>Don't have an account? <Link to="/register">Register</Link></span>
-      </div>
-    </div>
-  </Wrapper>
-);
+    </Wrapper>
+  );  
+};
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,20 @@
 // src/pages/Register.js
-import React from 'react';
-import InputField from '../components/InputField';
-import Button from '../components/Button';
+import React ,{ useState } from 'react';
 import Wrapper from '../components/Wrapper';
 import { Link } from 'react-router-dom';
 import '../styles/Register.css'; // Adjust the path as necessary
 
-const Register = () => (
+const Register = () => {
+  const [username , setUsername] = useState('');
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+  const [confirm_password , setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Username: ${username} Email: ${email} Password: ${password} Confirm Password: ${confirm_password}`);
+  }
+  return(
   <Wrapper>
     <div className="register_box">
       <div className="header">
@@ -14,16 +22,27 @@ const Register = () => (
         <br />
         <h4>Register</h4>
       </div>
-      <InputField type="text" id="username" placeholder="username" />
-      <InputField type="email" id="email" placeholder="Email" />
-      <InputField type="password" id="password" placeholder="Password" />
-      <InputField type="password" id="confirm_password" placeholder="Confirm Password" />
-      <Button type="submit" value="Register" />
+      <form onSubmit={handleSubmit}>
+        <div className="input_box">
+          <input type="text" id="Username" placeholder="username" className='input-field' onChange={(e)=>setUsername(e.target.value)} required/>
+        </div>
+        <div className="input_box">
+          <input type="email" id="email" placeholder="Email" className='input-field' onChange={(e)=>setEmail(e.target.value)} required/>
+        </div>
+        <div className="input_box">
+          <input type="password" id="password" placeholder="Password" className='input-field' onChange={(e)=>setPassword(e.target.value)} required/>
+        </div>
+        <div className="input_box">
+          <input type="password" id="confirm_password" placeholder="Confirm Password" className='input-field' onChange={(e)=>setConfirmPassword(e.target.value)} required/>
+        </div>
+        <input type="submit" value="Register" className = "input-submit"/>
+      </form>
       <div className="login">
         <span>Already have an account? <Link to="/login">Login</Link></span>
       </div>
     </div>
   </Wrapper>
-);
+  );
+};
 
 export default Register;
